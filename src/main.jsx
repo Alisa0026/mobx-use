c3bcf8145a2e00efbb0b5d4457cf805d6656fc7b
yql <yuanql@foxmail.com>
2023-03-24 16:56:06 +0800

    feat:可以尝试从main.jsx中debug源码

--- src/main.jsx
+++ src/main.jsx
@@ -6,3 +6,15 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 // root.render(<Counter />);
 root.render(<App />);
+
+
+// 可以对源码做个debugger
+/* import { observable, autorun } from 'mobx';
+let obj = { name: '1' }
+debugger
+let proxyObj = observable(obj);
+debugger
+autorun(() => {
+    console.log(proxyObj.name);
+})
+proxyObj.name = '2'; */
